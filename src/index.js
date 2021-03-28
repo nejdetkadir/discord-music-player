f484a3a925301c6f1eccd2a843a7eabb5b29cf69
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-28 19:49:47 +0300

    Show reaction when called prefix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello world :D");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+  if (!msg.author.bot && msg.content.startsWith(process.env.PREFIX)) {
+    msg.reply("i am here")
+  }
+});
+
+client.login(process.env.TOKEN);
