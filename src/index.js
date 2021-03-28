34c43cca5c6f8a59fed556dd37a7760bf1a4cd4e
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-28 21:17:57 +0300

    Create functionality of playing music

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
+const ytdl = require('ytdl-core');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', msg => {
-  if (!msg.author.bot && msg.content.startsWith(process.env.PREFIX)) {
+client.on('message', async msg => {
+  if (!msg.author.bot && msg.content.startsWith(process.env.PREFIX) && msg.guild) {
     const [COMMAND, ...args] = msg.content.trim().substring(process.env.PREFIX.length).split(/\s+/);
     if (COMMAND) {
-      if (args.length == 0) {
-        msg.reply("What do u want ?")
-      } else if (args.length == 1) {
-        msg.reply("i can play a song for you :)")
+      if (args.length == 1 && COMMAND == "play") {
+        if (msg.member.voice.channel) {
+          if (client.voice.connections.size == 0) {
+            if (ytdl.validateURL(args[0])) {
+              try {
+                const songInfo = await ytdl.getInfo(args[0])
+                const connection = await msg.member.voice.channel.join()
+                connection.play(ytdl(args[0], { filter: 'audioonly' }))
+                msg.reply(`Playing ${songInfo.videoDetails.title}`)
+              } catch (error) {
+                msg.reply("There is an problem :( i am researching...")
+              }
+            } else {
+              msg.reply("You need to check url!")
+            }
+          } else {
+            msg.reply("I am already working!")
+          }          
+        } else {
+          msg.reply('You need to join a voice channel first!');
+        }
       } else {
         msg.reply("i can't help you :(")
       }
