648d1d2d5d74f148a06d0aee46079d3973edf17c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-29 12:12:01 +0300

    Edit codes for codacy issues

--- src/helper.js
+++ src/helper.js
@@ -1,20 +1,20 @@
-const ytsr = require('ytsr');
-const ytdl = require('ytdl-core');
+const ytsr = require("ytsr");
+const ytdl = require("ytdl-core");
 
 exports.userOnVoiceChannel = (msg) => {
   if (msg.member.voice.channel) {
-    return true
+    return true;
   } else {
-    msg.reply('You need to join a voice channel first!');
+    msg.reply("You need to join a voice channel first!");
   }
 }
 
 exports.isWorking = (client, msg, showErr) => {
   if (client.voice.connections.size > 0) {
-    return true
+    return true;
   } else {
     if (showErr) {
-      msg.reply("I am not working!")
+      msg.reply("I am not working!");
     }
   }    
 }
@@ -23,33 +23,33 @@ exports.searchVideo = async (args) => {
   const searchText = args.join(" ");
   
   const searchFilter = await ytsr.getFilters(searchText);
-  const filterVideo = searchFilter.get('Type').get('Video');
+  const filterVideo = searchFilter.get("Type").get("Video");
 
   const result = await ytsr(filterVideo.url, { pages: 1 });
-  return result.items[0]
+  return result.items[0];
 }
 
 exports.playMusicWithUrl = async (url, msg, isWorking) => {
   if (ytdl.validateURL(url)) {
     if (isWorking) {
-      this.disconnectFromChannel(msg, false)
+      this.disconnectFromChannel(msg, false);
     }
     try {
       const songInfo = await ytdl.getInfo(url)
-      const connection = await msg.member.voice.channel.join()
-      connection.play(ytdl(url, { filter: 'audioonly' }))
-      msg.reply(`Playing ${songInfo.videoDetails.title}`)
+      const connection = await msg.member.voice.channel.join();
+      connection.play(ytdl(url, { filter: 'audioonly' }));
+      msg.reply(`Playing ${songInfo.videoDetails.title}`);
     } catch (error) {
-      msg.reply("There is an problem :( i am researching...")
+      msg.reply("There is an problem :( i am researching...");
     }
   } else {
-    msg.reply("You need to check url!")
+    msg.reply("You need to check url!");
   }
 }
 
 exports.disconnectFromChannel = (msg, showErr) => {
-  msg.guild.me.voice.channel.leave()
+  msg.guild.me.voice.channel.leave();
   if (showErr) {
-    msg.reply("Successfully disconnected")
+    msg.reply("Successfully disconnected");
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config()
+require("dotenv").config()
 const check = require("./helper")
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', msg => {
+client.on("message", msg => {
   if (!msg.author.bot && msg.content.startsWith(process.env.PREFIX) && msg.guild) {
     const [COMMAND, ...args] = msg.content.trim().substring(process.env.PREFIX.length).split(/\s+/);
     if (COMMAND) {
       if (args.length == 1 && COMMAND == "play") {
         if (check.userOnVoiceChannel(msg)) {
-          check.playMusicWithUrl(args[0], msg, check.isWorking(client, msg, false))  
+          check.playMusicWithUrl(args[0], msg, check.isWorking(client, msg, false));
         }
       } else if (args.length == 0 && (COMMAND == "disconnect" || COMMAND == "disc")) {
         if (check.isWorking(client, msg, true)) {
-          check.disconnectFromChannel(msg, true)
+          check.disconnectFromChannel(msg, true);
         }      
       } else if (args.length > 0 && COMMAND == "search") {
         if (check.userOnVoiceChannel(msg)) {
           check.searchVideo(args).then(res => {
-            check.playMusicWithUrl(res.url, msg, check.isWorking(client, msg, false))
-          })
+            check.playMusicWithUrl(res.url, msg, check.isWorking(client, msg, false));
+          });
         }        
       } else {
-        msg.reply("i can't help you :(")
+        msg.reply("i can't help you :(");
       }
     }
   }
